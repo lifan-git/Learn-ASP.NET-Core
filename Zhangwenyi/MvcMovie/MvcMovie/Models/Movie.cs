b4ccceb75666088bdaf04db3b1ca7a0095a4f355
GitHub <809002214@qq.com>
2020-02-05 20:03:15 +0800

    帮zhangwenyi提交，原因：网络问题无法提交。

---
+++ Zhangwenyi/MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { set; get; }
+        public string Title { set; get; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { set; get; }
+        public string Genre { set; get; }
+        public decimal Price { set; get; }
+    }
+}
