d53f0c85965f542b632e6a7c8460d10f5459b58d
chenjing <chenjing@scgathersoft.com>
2020-02-05 17:53:28 +0800

    提交今日DEMO，已完成ASP.NET Core 中的控制器方法和视图

---
+++ Chenjing/MvcMovie/Models/Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        [Display(Name = "电影名称")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 有效日期
+        /// </summary>
+        [DataType(DataType.Date)]
+        [Display(Name = "有效日期")]
+        public DateTime ReleaseDate { get; set; }
+
+        /// <summary>
+        /// 电影类型
+        /// </summary>
+        [Display(Name = "电影类型")]
+        public string Genre { get; set; }
+
+        /// <summary>
+        /// 票价
+        /// </summary>
+        [Display(Name = "票价")]
+        public decimal Price { get; set; } 
+    }
+}
---
+++ Chenjing/MvcMovie/Models/SendData.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public static class SendData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>())) 
+            {
+                if (context.Movie.Any()) 
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(new Movie()
+                {
+                    Title = "庆余年",
+                    Genre="科幻武侠",
+                    ReleaseDate =DateTime.Parse("2019/11/01"),
+                    Price = 124
+                },
+                new Movie()
+                {
+                    Title = "猫妖的诱惑",
+                    Genre = "动漫",
+                    ReleaseDate = DateTime.Parse("2019/02/01"),
+                    Price = 66
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
