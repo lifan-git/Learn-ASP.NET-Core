5d823098225750e15863dc2a1977a95c03ca27dd
GitHub <809002214@qq.com>
2020-02-06 11:58:23 +0800

    学习添加新字段、添加验证

--- Huangxingyao/MvcMovie/Controllers/MoviesController.cs
+++ Huangxingyao/MvcMovie/Controllers/MoviesController.cs
@@ -20,8 +20,11 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            // 使用LINQ获得流派列表。
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+
             var movies = from m in _context.Movie select m;
 
             if (!string.IsNullOrEmpty(searchString))
@@ -29,7 +32,18 @@ namespace MvcMovie.Controllers
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
-            return View(await movies.ToListAsync());
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         [HttpPost]
@@ -67,7 +81,7 @@ namespace MvcMovie.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -99,7 +113,7 @@ namespace MvcMovie.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (id != movie.Id)
             {
---
+++ Huangxingyao/MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    /// <summary>
+    /// 电影流派
+    /// </summary>
+    public class MovieGenreViewModel
+    {
+        /// <summary>
+        /// 电影列表
+        /// </summary>
+        public List<Movie> Movies { get; set; }
+
+        /// <summary>
+        /// 流派列表
+        /// </summary>
+        public SelectList Genres { get; set; }
+
+        /// <summary>
+        /// 所选流派
+        /// </summary>
+        public string MovieGenre { get; set; }
+
+        /// <summary>
+        /// 搜索文本
+        /// </summary>
+        public string SearchString { get; set; }
+    }
+}
