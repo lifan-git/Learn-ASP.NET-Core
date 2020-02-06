a8e394497859e7b9a1f05406a063536dc6faa322
zhangwenyiname <1335370124@qq.com>
2020-02-06 19:14:46 +0800

    添加搜索，验证等

--- Zhangwenyi/MvcMovie/MvcMovie/Models/Movie.cs
+++ Zhangwenyi/MvcMovie/MvcMovie/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,24 @@ namespace MvcMovie.Models
 {
     public class Movie
     {
+
         public int Id { set; get; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { set; get; }
 
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"),DataType(DataType.Date)]
         public DateTime ReleaseDate { set; get; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"),Required,StringLength(30)]
         public string Genre { set; get; }
+
+        [Range(1, 100), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { set; get; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+        public string Rating { set; get; }
     }
 }
